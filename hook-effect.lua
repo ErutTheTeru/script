2ffd7e02ecdd98321b54f57577f554ce30ec7c25
Teru <143543521+ErutTheTeru@users.noreply.github.com>
2024-08-21 17:05:05 +0700

    Update hook-effect.lua

--- hook-effect.lua
+++ hook-effect.lua
@@ -1,7 +1,11 @@
 if hookfunction and not islclosure(hookfunction) then 
-    for i, v in game:GetService("ReplicatedStorage").Assets.GUI:GetChildren() do 
-        v.Enabled = false 
-    end
+    fl { 
+        rs.Assets.GUI:GetChildren(),
+        function(_, v)
+            v.Enabled = false
+        end
+    }
+
     hookfunction(require(game:GetService("ReplicatedStorage").Effect.Container.Death), function() end)
     hookfunction(require(game:GetService("ReplicatedStorage").Effect.Container.Respawn), function() end)
 end
