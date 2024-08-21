c698857a24d050ba9c3542115e9b8b72b67f716b
Teru <143543521+ErutTheTeru@users.noreply.github.com>
2024-08-21 17:01:54 +0700

    Update blox-rejoin-skid.lua

--- blox-rejoin-skid.lua
+++ blox-rejoin-skid.lua
@@ -1,26 +1,26 @@
+lp.Idled:Connect(function()
+        game:GetService("VirtualUser"):Button2Down(Vector2.new(0,0), workspace.CurrentCamera.CFrame)
+        wt(1)
+        game:GetService("VirtualUser"):Button2Up(Vector2.new(0,0), workspace.CurrentCamera.CFrame)
+end)
 
-        game.Players.LocalPlayer.Idled:connect(function()
-            game:GetService("VirtualUser"):Button2Down(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
-            task.wait(1)
-            game:GetService("VirtualUser"):Button2Up(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
-        end)
-        function skid(v)
-            if v.Name == "ErrorPrompt" then
-                if v.Visible then
-                    if v.TitleFrame.ErrorTitle.Text ~= "Teleport Failed" then
-                        game:GetService("TeleportService"):Teleport(game.PlaceId, lp)
-                    end
-                end
-                v:GetPropertyChangedSignal("Visible"):Connect(function()
-                    if v.Visible then
+func {
+        getgenv(),
+        "rejoin",
+        function(v)
+                if v.Name == "ErrorPrompt" then
                         if v.TitleFrame.ErrorTitle.Text ~= "Teleport Failed" then
-                            game:GetService("TeleportService"):Teleport(game.PlaceId, lp)
+                                game:GetService("TeleportService"):Teleport(game.PlaceId, lp)
                         end
-                    end
-                end)
-            end
+                end
         end
-        for i, v in game.CoreGui.RobloxPromptGui.promptOverlay:GetChildren() do
-            skid(v) 
-        end
-        game.CoreGui.RobloxPromptGui.promptOverlay.ChildAdded:Connect(skid)
+}
+
+fl {
+        game.CoreGui.RobloxPromptGui.promptOverlay:GetChildren(),
+        function(_, v)
+                rejoin(v)
+        end 
+}
+
+CoreGui.RobloxPromptGui.promptOverlay.ChildAdded:Connect(rejoin)
