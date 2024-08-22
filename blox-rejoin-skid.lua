2a0b0c2b512cb28dff9d52b24b6edb69fe873b58
Teru <143543521+ErutTheTeru@users.noreply.github.com>
2024-08-22 18:20:06 +0700

    Update blox-rejoin-skid.lua

--- blox-rejoin-skid.lua
+++ blox-rejoin-skid.lua
@@ -1,25 +1,27 @@
-lp.Idled:Connect(function()
-        game:GetService("VirtualUser"):Button2Down(Vector2.new(0,0), workspace.CurrentCamera.CFrame)
-        game:GetService("VirtualUser"):Button2Up(Vector2.new(0,0), workspace.CurrentCamera.CFrame)
-end)
-
-func {
-        bounty.functions.misc,
-        "rejoin",
-        function(v)
-                if v.Name == "ErrorPrompt" then
-                        if v.TitleFrame.ErrorTitle.Text ~= "Teleport Failed" then
-                                game:GetService("TeleportService"):Teleport(game.PlaceId, lp)
+spawn(function()
+        lp.Idled:Connect(function()
+                game:GetService("VirtualUser"):Button2Down(Vector2.new(0,0), workspace.CurrentCamera.CFrame)
+                game:GetService("VirtualUser"):Button2Up(Vector2.new(0,0), workspace.CurrentCamera.CFrame)
+        end)
+        
+        func {
+                bounty.functions.misc,
+                "rejoin",
+                function(v)
+                        if v.Name == "ErrorPrompt" and ffc(v, 'MessageArea') and ffc(v.MessageArea, "ErrorFrame") then
+                                if v.TitleFrame.ErrorTitle.Text ~= "Teleport Failed" then
+                                        game:GetService("TeleportService"):Teleport(game.PlaceId, lp)
+                                end
                         end
                 end
-        end
-}
-
-fl {
-        CoreGui.RobloxPromptGui.promptOverlay:GetChildren(),
-        function(_, v)
-                rejoin(v)
-        end 
-}
-
-CoreGui.RobloxPromptGui.promptOverlay.ChildAdded:Connect(rejoin)
+        }
+        
+        fl {
+                CoreGui.RobloxPromptGui.promptOverlay:GetChildren(),
+                function(_, v)
+                        rejoin(v)
+                end 
+        }
+        
+        CoreGui.RobloxPromptGui.promptOverlay.ChildAdded:Connect(rejoin)
+end)
