394edabc18c3de720c504c145836fa82e7f6e877
Teru <143543521+ErutTheTeru@users.noreply.github.com>
2024-08-22 19:51:24 +0700

    Update scripts-canuse.lua

--- scripts-canuse.lua
+++ scripts-canuse.lua
@@ -1,1 +1,1 @@
-return {bounty = {canuse = false, kick = "closing for update, join discord: discord.gg/CejANXn8sa"}, main = {canuse = false, kick = "not able to use"}}
+return {bounty = {canuse = true, kick = "closing for update, join discord: discord.gg/CejANXn8sa"}, main = {canuse = false, kick = "not able to use"}}
