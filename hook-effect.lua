8ef819f9026c8d1ae4d55efb56af823e1df915c9
Teru <143543521+ErutTheTeru@users.noreply.github.com>
2024-08-24 22:09:40 +0700

    Update hook-effect.lua

--- hook-effect.lua
+++ hook-effect.lua
@@ -4,22 +4,4 @@ if hookfunction and not islclosure(hookfunction) then
         end
         hookfunction(require(game:GetService("ReplicatedStorage").Effect.Container.Death), function() end)
         hookfunction(require(game:GetService("ReplicatedStorage").Effect.Container.Respawn), function() end)
-        task.spawn(function()
-                wait(1)
-                repeat task.wait(1) until game.Players.LocalPlayer.Character:FindFirstChildOfClass('Tool') and game.Players.LocalPlayer.Character:FindFirstChildOfClass('Tool').ToolTip == 'Melee'
-                for i,v in pairs(getupvalues(require(game:GetService("Players").LocalPlayer.PlayerScripts.CombatFramework))[2].activeController.data) do  
-                    if typeof(v) == 'function' then 
-                        hookfunction(v,function() end)
-                    end
-                end 
-        end) 
-        task.delay(10,function()
-                for i,v2 in pairs(game.ReplicatedStorage.Effect.Container:GetDescendants()) do 
-                    pcall(function()
-                        if v2.ClassName =='ModuleScript' and typeof(require(v2)) == 'function' then 
-                            hookfunction(require(v2),function()end)
-                        end
-                    end)
-                end
-        end)
 end
