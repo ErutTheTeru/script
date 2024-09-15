df37ca7d96f54dd711ed53c02aff0dafc4038b76
Teru <143543521+ErutTheTeru@users.noreply.github.com>
2024-09-15 16:34:29 +0700

    Update blox-rejoin-skid.lua

--- blox-rejoin-skid.lua
+++ blox-rejoin-skid.lua
@@ -1,17 +1,16 @@
 spawn(function()
         lp.Idled:Connect(function()
-                game:GetService("VirtualUser"):Button2Down(Vector2.new(0,0), workspace.CurrentCamera.CFrame)
+                game:GetService('VirtualUser'):Button2Down(Vector2.new(0,0), workspace.CurrentCamera.CFrame)
                 wt(1)
-                game:GetService("VirtualUser"):Button2Up(Vector2.new(0,0), workspace.CurrentCamera.CFrame)
+                game:GetService('VirtualUser'):Button2Up(Vector2.new(0,0), workspace.CurrentCamera.CFrame)
         end)
         
-        func {
-                bounty.functions.misc,
-                "rejoin",
+        func 
+                'rejoin',
                 function(v)
-                        if v.Name == "ErrorPrompt" and ffc(v, 'MessageArea') and ffc(v.MessageArea, "ErrorFrame") and not bounty.hop then
-                                if v.TitleFrame.ErrorTitle.Text ~= "Teleport Failed" then
-                                        game:GetService("TeleportService"):Teleport(game.PlaceId, lp)
+                        if v.Name == 'ErrorPrompt' and ffc(v, 'MessageArea') and ffc(v.MessageArea, 'ErrorFrame') and not bounty.hop then
+                                if v.TitleFrame.ErrorTitle.Text ~= 'Teleport Failed' then
+                                        game:GetService('TeleportService'):Teleport(game.PlaceId, lp)
                                 end
                         end
                 end
