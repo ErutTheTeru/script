0f0c2582a94059bfdd8315e6cc9093ae26949e0c
Teru <143543521+ErutTheTeru@users.noreply.github.com>
2024-10-24 21:52:01 +0700

    Update scripts-canuse.lua

--- scripts-canuse.lua
+++ scripts-canuse.lua
@@ -1,5 +1,5 @@
 return {
-    bounty = {canuse = false, kick = "discontinued, join discord: discord.gg/CejANXn8sa to get newest script"}, 
+    bounty = {canuse = false, kick = "updated, join discord: discord.gg/CejANXn8sa to get newest script"}, 
     bountyhavekey = {canuse = false, kick = "discontinued, discord.gg/CejANXn8sa"}, 
     main = {canuse = false, kick = "not able to use"}
 }
